8abc940878b1a74bd0b99be59a6f36aae101f9f0
Carlos Padilha <cpadilha38@outlook.com.br>
2020-05-07 19:51:26 -0300

    cuidadis com numeros

---
+++ exercicios-js/fundamentos/numerosCuidados.js
@@ -0,0 +1,4 @@
+console.log(7 / 0) // retorna infinito
+console.log("10" / 2) /* verifica se a string pode ser 
+convertida para numero e realiza a divisão */
+console.log("Show!" * 2) // gera um "não é numero"
