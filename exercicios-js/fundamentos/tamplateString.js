34334b1cb4100b7bfb37e4d93e8ebbfae112529e
Carlos Padilha <cpadilha38@outlook.com.br>
2020-05-11 10:35:54 -0300

    uso taplate strig

---
+++ exercicios-js/fundamentos/tamplateString.js
@@ -0,0 +1,11 @@
+const nome = "rebeca"
+const concat = "ola" + nome + "!"
+
+const template = `Olá // para declarar um tamplate string usamos o simbolo de crase
+${nome}`
+
+console.log(concat)
+console.log(template)
+
+console.log(`1 + 1 = ${1 + 1}`) // permite o uso de espreções matematicas e funções
+
